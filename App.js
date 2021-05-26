518aa42114d4971b058b366f1e4c5363a53d4269
elroygohjy <E0543508@u.nus.edu>
2021-05-26 19:16:55 +0800

    feat(Stack): Stack for SignUpScreen and SignInScreen

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react'
+import { NavigationContainer} from "@react-navigation/native"
+import {createStackNavigator} from "@react-navigation/stack"
+import LoginScreen from "./src/screens/LoginScreen"
+import firebase from "./api/authkey"
+const Stack = createStackNavigator()
+import SignUpScreen from "./src/screens/SignUpScreen"
+const screens = [
+    {name: "Login", component: LoginScreen},
+    {name: "SignUp", component: SignUpScreen}
+]
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName={screens[0].name} >
+                <Stack.Screen name={"Login"} component={LoginScreen}/>
+                <Stack.Screen name={"SignUp"} component={SignUpScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
